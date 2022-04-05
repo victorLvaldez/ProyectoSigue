d1d0349025524608e85ec09cc0c0dae8e003b1c2
Victor Lopez <victorrubenlopezvaldez@gmail.com>
2022-04-05 18:40:42 -0500

    Conection has been modified, not tested yet

--- config/firebase.js
+++ config/firebase.js
@@ -2,15 +2,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore" 
+const GLOBAL = require('../app.config')
+
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAML8Q2ZwFPAOKffW1kVfHi1q2a176ekvg",
-    authDomain: "proyectosigueapp.firebaseapp.com",
-    projectId: "proyectosigueapp",
-    storageBucket: "proyectosigueapp.appspot.com",
-    messagingSenderId: "283332517535",
-    appId: "1:283332517535:web:790f4105d00a7554794caa",
-    measurementId: "G-5TZQ5SKMML"
+    apiKey: GLOBAL.default.extra.apiKey,
+    authDomain: GLOBAL.default.extra.authDomain,
+    projectId: GLOBAL.default.extra.projectId,
+    storageBucket: GLOBAL.default.extra.storageBucket,
+    messagingSenderId: GLOBAL.default.extra.messagingSenderId,
+    appId: GLOBAL.default.extra.appId,
+    measurementId: GLOBAL.default.extra.measurementId
 };
 
 // Initialize Firebase
